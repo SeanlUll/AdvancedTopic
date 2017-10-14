a10118d0ab7dbef1c1fe48c082081f1aa61b0b15
SeanlUll <seanhe@lullgroup.com>
2017-10-14 15:42:57 +0800

    chapter 3

---
+++ Chapter3_ObjectIterator/3_2_CityWeather.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/14 15:25
+# @Author  : Sean
+# @File    : 3_2_CityWeather.py
---
+++ Chapter3_ObjectIterator/3_3_PrimeNumbers.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/14 15:41
+# @Author  : Sean
+# @File    : 3_3_PrimeNumbers.py
