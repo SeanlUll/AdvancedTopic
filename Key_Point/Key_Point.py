b2c8e66c2dd86dc0b2f88701a0381bad4ba8ebb1
SeanlUll <seanhe@lullgroup.com>
2017-10-25 17:45:38 +0800

    key point

--- Key_Point/Key_Point.py
+++ Key_Point/Key_Point.py
@@ -128,3 +128,81 @@ sys modules
 print("*" * 80)
 import sys
 print("\n".join(sys.modules.keys()))
+print(dir(__builtins__))
+
+
+print("*" * 80)
+def extendList(val, list=[]):
+    list.append(val)
+    return list
+
+list1 = extendList(10)
+list2 = extendList(123,[])
+list3 = extendList('a')
+
+print("list1 = %s" % list1)
+print("list2 = %s" % list2)
+print("list3 = %s" % list3)
+
+
+print("*" * 80)
+def extendList(val, list=None):
+    if list is None:
+        list = []
+    list.append(val)
+    return list
+
+list1 = extendList(10)
+list2 = extendList(123,[])
+list3 = extendList('a')
+
+print("list1 = %s" % list1)
+print("list2 = %s" % list2)
+print("list3 = %s" % list3)
+
+print("*" * 80)
+def multipliers():
+    return [lambda x : i * x for i in range(4)]
+
+print([m(2) for m in multipliers()]) #[6, 6, 6, 6]
+
+print("*" * 80)
+def multipliers():
+    for i in range(4):
+        yield lambda x : i * x
+print([m(2) for m in multipliers()]) #[0, 2, 4, 6]
+
+print("*" * 80)
+class Parent(object):
+    x = 1
+class Child1(Parent):
+    pass
+class Child2(Parent):
+    pass
+
+print (Parent.x, Child1.x, Child2.x)
+Child1.x = 2
+print (Parent.x, Child1.x, Child2.x)
+Parent.x = 3
+print (Parent.x, Child1.x, Child2.x)
+
+
+print("*" * 80)
+def div1(x,y):
+    print("%s/%s = %s" % (x, y, x/y))
+
+def div2(x,y):
+    print("%s//%s = %s" % (x, y, x//y))
+
+div1(5,2)
+div1(5.,2)
+div2(5,2)
+div2(5.,2.)
+
+print("*" * 80)
+list = ['a', 'b', 'c', 'd', 'e']
+print(list[10:])
+
+print("*" * 80)
+list = [[]] * 5
+print(list)
